6076787a0f0102f5c2f818185d49121e780a23c8
yampieriphone10-lab <yampieriphone10@gmail.com>
2026-09-11 08:04:44 -0500

    Delete Launcher.java

--- Launcher.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upeu.repasog1array;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
